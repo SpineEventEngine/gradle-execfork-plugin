2fd93cd98baa9a3472dc6cb7b79eb79ae543a9e3
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-09-26 05:10:05 +0300

    Add trailing periods in parameter docs

--- src/main/kotlin/io/spine/tools/gradle/exec/PortUtils.kt
+++ src/main/kotlin/io/spine/tools/gradle/exec/PortUtils.kt
@@ -31,17 +31,19 @@ import java.net.*
 import java.util.concurrent.TimeUnit
 
 /**
- * Find a random port that is available to listen on
- * @return a port number that is available
+ * Find a random port that is available to listen on.
+ *
+ * @return a port number that is available.
  */
 fun findOpenPort(): Int {
     ServerSocket(0).use { return it.localPort }
 }
 
 /**
- * Check if a given port is in use
- * @param port the port number to check
- * @return true if the port is open, false otherwise
+ * Check if a given port is in use.
+ *
+ * @param port the port number to check.
+ * @return `true` if the port is open, `false` otherwise.
  */
 private fun isPortOpen(port: Int): Boolean {
     Socket().use {
@@ -58,19 +60,20 @@ private fun isPortOpen(port: Int): Boolean {
 
 /**
  * Wait for a given amount of time for a port to be opened locally, by a given
- * process. This method will poll every 100 ms and try to connect to the port. If
- * the amount of time is reached and the port is not yet open, a GradleException
- * is thrown. If the given process terminates before the port is open, a
- * GradleException is thrown.
+ * process. This method will poll every 100 ms and try to connect to the port.
  *
- * @param port the port number to check
- * @param timeout the maximum number of TimeUnits to wait
- * @param unit the unit of time to wait
- * @param process the process to monitor for early termination
+ * If the amount of time is reached and the port is not yet open, a `GradleException`
+ * is thrown. If the given process terminates before the port is open, a
+ * `GradleException` is thrown.
+ *
+ * @param port the port number to check.
+ * @param timeout the maximum number of `TimeUnits` to wait.
+ * @param unit the unit of time to wait.
+ * @param process the process to monitor for early termination.
  *
  * @throws GradleException when the timeout has elapsed and the port is still
  *          not open, OR the given process has terminated before the port is
- *          opened (whichever occurs first)
+ *          opened (whichever occurs first).
  */
 fun waitForPortOpen(port: Int, timeout: Long, unit: TimeUnit, process: Process) {
     val millisToWait: Long = unit.toMillis(timeout)
